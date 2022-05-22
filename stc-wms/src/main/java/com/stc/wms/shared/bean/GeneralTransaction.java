1df50a2a98ecb56e60f81b3705a7c8bf30757996
Job Gonzalez <jgonzalez@bytesw.com>
2022-05-22 15:03:32 -0600

    interface y Ben de transacción general para heredar

---
+++ stc-wms/src/main/java/com/stc/wms/shared/bean/GeneralTransaction.java
@@ -0,0 +1,12 @@
+package com.stc.wms.shared.bean;
+
+/**
+ * User: job
+ * Date: 21/05/22
+ * Time: 20:33
+ *
+ * @author job
+ */
+public interface GeneralTransaction {
+    void doAccepConfirm();
+}
