66c91ae2c2c9248b0fae27446a553902e14a947d
Job Gonzalez <jgonzalez@bytesw.com>
2022-05-22 15:04:01 -0600

    clases para uso uso de perfiles desde frontEnd

---
+++ stc-wms/src/main/java/com/stc/wms/security/service/ManagerProfileService.java
@@ -0,0 +1,56 @@
+package com.stc.wms.security.service;
+
+import com.stc.wms.security.controller.ProfileController;
+import com.stc.wms.security.dto.ProfileDTO;
+import feign.RetryableException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * User: job
+ * Date: 21/05/22
+ * Time: 23:18
+ *
+ * @author job
+ */
+@Slf4j
+@Service("profileManager")
+public class ManagerProfileService {
+
+    private final ProfileController profileController;
+
+    @Autowired
+    public ManagerProfileService(ProfileController controller){
+        this.profileController = controller;
+    }
+
+    public ProfileDTO savedProfile(ProfileDTO service){
+        try {
+            ResponseEntity<ProfileDTO> save = this.profileController.saveProfile(service);
+            return Objects.requireNonNull(save.getBody());
+        }catch (RetryableException ex){
+            log.error("Error consumiengo servicio cliente",ex);
+            ProfileDTO errorReturned = new ProfileDTO();
+            errorReturned.setErrorCode(1);
+            errorReturned.setErrorMessage("Error almacenando la informacion");
+            return errorReturned;
+        }
+    }
+
+    public List<ProfileDTO> loadAllProfile(){
+        try{
+            ResponseEntity<List<ProfileDTO>> load = this.profileController.loadAll();
+            return load.getBody();
+        }catch (RetryableException ex){
+            log.error("Error consumiendo servicio ",ex);
+            return null;
+        }
+    }
+
+
+}
