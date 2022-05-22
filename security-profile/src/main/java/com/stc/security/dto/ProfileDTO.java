0bdb392a1a8e11abd4b58389d508cad051bfece3
Job Gonzalez <jgonzalez@bytesw.com>
2022-05-22 15:02:17 -0600

    dto  para perfiles en servcicio security

---
+++ security-profile/src/main/java/com/stc/security/dto/ProfileDTO.java
@@ -0,0 +1,23 @@
+package com.stc.security.dto;
+
+import com.stc.security.model.AuthorizationService;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * User: job
+ * Date: 21/05/22
+ * Time: 23:15
+ *
+ * @author job
+ */
+@Data
+public class ProfileDTO {
+    private int errorCode;
+    private String errorMessage;
+    private int perfilId;
+    private String name;
+    private String description;
+    private List<AuthorizationService> serviceList;
+}
